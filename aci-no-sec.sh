55ca84b48dc715d844c583cb7d0cbcf810376c9f
Oleksandr Kreschenko <Alexander.Kross@gmail.com>
2018-01-24 14:52:42 +0200

    aci-no-sec finalized.

--- aci-no-sec.sh
+++ aci-no-sec.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/jq -f
-#!/usr/bin/env sh
-#egrep -v '"pwd": |"key": |"cert": |"identityPrivateKeyContents": |"identityPrivateKeyPassphrase": |"identityPublicKeyContents": |"userPasswd": |"password": |"authKey": |"value": "SES1' $*
-## shorter:
-#egrep -v '^[[:space:]]*"[[:alpha:]]+":[[:space:]]*"SES1' $*
-## both variants are wrong, because does not remove preceding comma if any.
-#del(.pwd)|del(.key)|del(.cert)|del(.identityPrivateKeyContents)|del(.identityPrivateKeyPassphrase)|del(.identityPublicKeyContents)|del(.userPasswd)|del(.password)|del(.authKey)
-#del(.pwd),del(.key),del(.cert),del(.identityPrivateKeyContents),del(.identityPrivateKeyPassphrase),del(.identityPublicKeyContents),del(.userPasswd),del(.password),del(.authKey)
-#del(.value) which begins with "SES1..."
 def walk(f):
-  . as $in
-  | if type == "object" then
+  . as $in |
+  if type == "object" then
       reduce keys_unsorted[] as $key
         ( {}; . + { ($key):  ($in[$key] | walk(f)) } ) | f
   elif type == "array" then map( walk(f) ) | f
   else f
   end;
-walk(if type == "object" then del(.pwd)|del(.key)|del(.cert)|del(.identityPrivateKeyContents)|del(.identityPrivateKeyPassphrase)|del(.identityPublicKeyContents)|del(.userPasswd)|del(.password)|del(.authKey) else . end)
-# ToDo: improve with test() to filter out .value containing "^SES1"
+walk(
+  # wildcard variant like this: if type == "string" and startswith("SES1") then del(.) else . end # maybe null instead of del(.)
+  if type == "object" then
+    del(.pwd)|del(.key)|del(.cert)|del(.identityPrivateKeyContents)|del(.identityPrivateKeyPassphrase)|del(.identityPublicKeyContents)|del(.userPasswd)|del(.password)|del(.authKey)|
+    if has("value") and (.value|type == "string") and (.value|startswith("SES1")) then # shorter did not work: if (.value|startswith("SES1")?) then
+      del(.value)
+    else .
+    end
+  else .
+  end
+)
+## Test with:
+#echo '{"tmp": 2, "value":"SES1sgls", "var":[]}' | jq 'if has("value") and (.value|type == "string") and (.value|startswith("SES1")) then del(.value) else [., (.value|type)] end'
+#{
+#  "tmp": 2,
+#  "var": []
+#}
